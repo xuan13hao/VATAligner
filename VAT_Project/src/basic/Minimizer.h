2403f0230eef5a7f3c286eba3e3a9577f8437679
xuan13hao <xuan@DESKTOP-3IDD8L2>
2023-03-29 13:28:11 -0500

    changeminimizers

--- VAT_Project/src/basic/Minimizer.h
+++ VAT_Project/src/basic/Minimizer.h
@@ -15,55 +15,40 @@
  *               Callers may want to set "p->n = 0"; otherwise results are appended to p
  */
 
-void mm_sketch(const char *str, int len, int w, int k, uint32_t rid, mm128_v *p)
-{
-	uint64_t shift1 = 2 * (k - 1), mask = (1ULL<<2*k) - 1, kmer[2] = {0,0};
-	int i, j, l, buf_pos, min_pos;
-	mm128_t *buf, min = { UINT64_MAX, UINT64_MAX };
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
-	assert(len > 0 && w > 0 && k > 0);
-	buf = (mm128_t*)alloca(w * 16);
-	memset(buf, 0xff, w * 16);
+using namespace std;
 
-	for (i = l = buf_pos = min_pos = 0; i < len; ++i) {
-		int c = seq_nt4_table[(uint8_t)str[i]];
-		mm128_t info = { UINT64_MAX, UINT64_MAX };
-		if (c < 4) { // not an ambiguous base
-			int z;
-			kmer[0] = (kmer[0] << 2 | c) & mask;           // forward k-mer
-			kmer[1] = (kmer[1] >> 2) | (3ULL^c) << shift1; // reverse k-mer
-			if (kmer[0] == kmer[1]) continue; // skip "symmetric k-mers" as we don't know it strand
-			z = kmer[0] < kmer[1]? 0 : 1; // strand
-			if (++l >= k)
-				info.x = hash64(kmer[z], mask), info.y = (uint64_t)rid<<32 | (uint32_t)i<<1 | z;
-		} else l = 0;
-		buf[buf_pos] = info; // need to do this here as appropriate buf_pos and buf[buf_pos] are needed below
-		if (l == w + k - 1) { // special case for the first window - because identical k-mers are not stored yet
-			for (j = buf_pos + 1; j < w; ++j)
-				if (min.x == buf[j].x && buf[j].y != min.y) kv_push(mm128_t, *p, buf[j]);
-			for (j = 0; j < buf_pos; ++j)
-				if (min.x == buf[j].x && buf[j].y != min.y) kv_push(mm128_t, *p, buf[j]);
-		}
-		if (info.x <= min.x) { // a new minimum; then write the old min
-			if (l >= w + k) kv_push(mm128_t, *p, min);
-			min = info, min_pos = buf_pos;
-		} else if (buf_pos == min_pos) { // old min has moved outside the window
-			if (l >= w + k - 1) kv_push(mm128_t, *p, min);
-			for (j = buf_pos + 1, min.x = UINT64_MAX; j < w; ++j) // the two loops are necessary when there are identical k-mers
-				if (min.x >= buf[j].x) min = buf[j], min_pos = j; // >= is important s.t. min is always the closest k-mer
-			for (j = 0; j <= buf_pos; ++j)
-				if (min.x >= buf[j].x) min = buf[j], min_pos = j;
-			if (l >= w + k - 1) { // write identical k-mers
-				for (j = buf_pos + 1; j < w; ++j) // these two loops make sure the output is sorted
-					if (min.x == buf[j].x && min.y != buf[j].y) kv_push(mm128_t, *p, buf[j]);
-				for (j = 0; j <= buf_pos; ++j)
-					if (min.x == buf[j].x && min.y != buf[j].y) kv_push(mm128_t, *p, buf[j]);
-			}
-		}
-		if (++buf_pos == w) buf_pos = 0;
-	}
-	if (min.x != UINT64_MAX)
-		kv_push(mm128_t, *p, min);
+vector<string> get_kmers(string sequence, int k) {
+    vector<string> kmers;
+
+    for (int i = 0; i <= sequence.length() - k; i++) {
+        string kmer = sequence.substr(i, k);
+        kmers.push_back(kmer);
+    }
+
+    return kmers;
+}
+
+string get_minimizer(string sequence, int k, int w) {
+    vector<string> kmers = get_kmers(sequence, k);
+    vector<string> window_kmers;
+    string minimizer;
+
+    for (int i = 0; i <= kmers.size() - w; i++) {
+        window_kmers = vector<string>(kmers.begin() + i, kmers.begin() + i + w);
+        sort(window_kmers.begin(), window_kmers.end());
+        string window_minimizer = window_kmers[0];
+
+        if (minimizer.empty() || window_minimizer < minimizer) {
+            minimizer = window_minimizer;
+        }
+    }
+
+    return minimizer;
 }
 
 #endif // __MINIMIZER_H__
