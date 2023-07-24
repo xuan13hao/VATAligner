a0c6c5c3d00b9e8c0888baff628162054178304a
xuan13hao <xuan@DESKTOP-3IDD8L2>
2023-07-24 15:48:54 -0500

    shapes

--- VAT_Project/src/basic/VATConsts.h
+++ VAT_Project/src/basic/VATConsts.h
@@ -14,8 +14,8 @@ class VATConsts
 		max_seed_weight = 32,
 		seqp_bits = 8,
 		seqp = 1<<seqp_bits,
-		max_shapes = 5,
-		index_modes = 3,
+		max_shapes = 1,
+		index_modes = 1,
 		min_shape_len = 10,
 		max_shape_len = 32,
 		seed_anchor = 8
