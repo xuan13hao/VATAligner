48c282f007a4e4293c8f2cbf66eba232c0889948
xuan13hao <xuan13hao@gmail.com>
2024-09-03 15:54:25 -0500

    data preparation

---
+++ build/CMakeFiles/VAT.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/xuan/VATAligner/src/Commons/VATParameters.cpp" "CMakeFiles/VAT.dir/src/Commons/VATParameters.cpp.o" "gcc" "CMakeFiles/VAT.dir/src/Commons/VATParameters.cpp.o.d"
+  "/home/xuan/VATAligner/src/VATMain.cpp" "CMakeFiles/VAT.dir/src/VATMain.cpp.o" "gcc" "CMakeFiles/VAT.dir/src/VATMain.cpp.o.d"
+  "/home/xuan/VATAligner/src/tools/sort_simd/avx256/Utils.cpp" "CMakeFiles/VAT.dir/src/tools/sort_simd/avx256/Utils.cpp.o" "gcc" "CMakeFiles/VAT.dir/src/tools/sort_simd/avx256/Utils.cpp.o.d"
+  "/home/xuan/VATAligner/src/tools/sort_simd/avx256/merge_util.cpp" "CMakeFiles/VAT.dir/src/tools/sort_simd/avx256/merge_util.cpp.o" "gcc" "CMakeFiles/VAT.dir/src/tools/sort_simd/avx256/merge_util.cpp.o.d"
+  "/home/xuan/VATAligner/src/tools/sort_simd/avx256/simd_sort.cpp" "CMakeFiles/VAT.dir/src/tools/sort_simd/avx256/simd_sort.cpp.o" "gcc" "CMakeFiles/VAT.dir/src/tools/sort_simd/avx256/simd_sort.cpp.o.d"
+  "/home/xuan/VATAligner/src/tools/sort_simd/avx256/sort_util.cpp" "CMakeFiles/VAT.dir/src/tools/sort_simd/avx256/sort_util.cpp.o" "gcc" "CMakeFiles/VAT.dir/src/tools/sort_simd/avx256/sort_util.cpp.o.d"
+  "/home/xuan/VATAligner/src/tools/sort_simd/common.cpp" "CMakeFiles/VAT.dir/src/tools/sort_simd/common.cpp.o" "gcc" "CMakeFiles/VAT.dir/src/tools/sort_simd/common.cpp.o.d"
+  "/home/xuan/VATAligner/src/tools/tinythread.cpp" "CMakeFiles/VAT.dir/src/tools/tinythread.cpp.o" "gcc" "CMakeFiles/VAT.dir/src/tools/tinythread.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/xuan/VATAligner/build/src/algo/blast/core/CMakeFiles/blast_core.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
